331a10a20854ddb62d68d0904a55f8db3f4985d0
Calvin Cheng <linchuan.cheng@gmail.com>
2015-03-09 14:27:19 +0800

    Memorable Places

--- Memorable Places/ViewController.swift
+++ Memorable Places/ViewController.swift
@@ -7,17 +7,62 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+var manager:CLLocationManager!
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var myMap: MKMapView!
+    
+    @IBAction func findMe(sender: AnyObject) {
+        println("findMe")
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println(activePlace)
+        
+        let lat = NSString(string: places[activePlace]["lat"]!).doubleValue
+        let lon = NSString(string: places[activePlace]["lon"]!).doubleValue
+        
+        var latitude:CLLocationDegrees = lat
+        var longitude: CLLocationDegrees = lon
+        var latDelta: CLLocationDegrees = 0.01
+        var lonDelta: CLLocationDegrees = 0.01
+        var span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        myMap.setRegion(region, animated: true)
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var userLocation:CLLocation = locations[0] as CLLocation
+        
+        var latitude:CLLocationDegrees = userLocation.coordinate.latitude
+        var longitude: CLLocationDegrees = userLocation.coordinate.longitude
+        println(latitude)
+        println(longitude)
+        var latDelta: CLLocationDegrees = 0.01
+        var lonDelta: CLLocationDegrees = 0.01
+        var span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        myMap.setRegion(region, animated: true)
     }
 
 
